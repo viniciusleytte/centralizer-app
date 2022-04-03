e5edd85eeacae11545309a0a443d5e25cdd137b4
Vinicius Leite <viniciusleytte@gmail.com>
2022-04-03 15:38:54 -0300

    Init project

---
+++ src/main/java/br/com/vinicius/centralizer/centralizerapp/CentralizerAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.centralizer.centralizerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vinicius/centralizer/centralizerapp/CentralizerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.centralizer.centralizerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
